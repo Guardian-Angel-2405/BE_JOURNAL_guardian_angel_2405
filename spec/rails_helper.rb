85607edea053a2d31ee57d0009978f4c83d1fc8d
Lance Nelson <lancen04@gmail.com>
2024-09-13 16:51:03 -0400

    added factory bot, gems, and model validations/spec test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,9 +63,16 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://rspec.info/features/7-0/rspec-rails
   config.infer_spec_type_from_file_location!
-
+  config.include FactoryBot::Syntax::Methods
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
